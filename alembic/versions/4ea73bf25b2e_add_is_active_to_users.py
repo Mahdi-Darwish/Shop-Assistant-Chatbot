c62668abcf3899bfb7779b2496058749b6a21a2a
Mahdi Darwish <mahdidarwish@Mahdis-MacBook-Pro.local>
2026-09-11 01:39:18 +0300

    edited docker-compose.yaml file

--- alembic/versions/4ea73bf25b2e_add_is_active_to_users.py
+++ alembic/versions/4ea73bf25b2e_add_is_active_to_users.py
@@ -1,3 +1,34 @@
+# """add is_active to users
+
+# Revision ID: 4ea73bf25b2e
+# Revises: b54c5af2a65c
+# Create Date: 2026-09-07 12:23:30.458963
+
+# """
+# from typing import Sequence, Union
+
+# from alembic import op
+# import sqlalchemy as sa
+
+
+# # revision identifiers, used by Alembic.
+# revision: str = '4ea73bf25b2e'
+# down_revision: Union[str, Sequence[str], None] = 'b54c5af2a65c'
+# branch_labels: Union[str, Sequence[str], None] = None
+# depends_on: Union[str, Sequence[str], None] = None
+
+
+# def upgrade() -> None:
+#     op.alter_column('users', 'is_active',
+#                existing_type=sa.BOOLEAN(),
+#                nullable=False,
+#                server_default=sa.text('true'))
+
+# def downgrade() -> None:
+#     op.alter_column('users', 'is_active',
+#                existing_type=sa.BOOLEAN(),
+#                nullable=True,
+#                server_default=None)
 """add is_active to users
 
 Revision ID: 4ea73bf25b2e
@@ -19,13 +50,10 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.alter_column('users', 'is_active',
-               existing_type=sa.BOOLEAN(),
-               nullable=False,
-               server_default=sa.text('true'))
+    op.add_column(
+        'users',
+        sa.Column('is_active', sa.Boolean(), nullable=False, server_default=sa.text('true')),
+    )
 
 def downgrade() -> None:
-    op.alter_column('users', 'is_active',
-               existing_type=sa.BOOLEAN(),
-               nullable=True,
-               server_default=None)
+    op.drop_column('users', 'is_active')
